6deab7c71fcee57b6dafc4c9e4520b75d92b5865
Alberto Ponces <ponces26@gmail.com>
2025-06-25 14:43:20 +0000

    extra: Build AOSPA's face unlock implementation

--- product.mk
+++ product.mk
@@ -31,6 +31,18 @@ PRODUCT_PACKAGES += \
 # Contextual Search
 PRODUCT_COPY_FILES += $(EXTRA_PATH)/etc/sysconfig/contextual_search.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/contextual_search.xml
 
+# Face Unlock
+ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
+PRODUCT_PACKAGES += \
+    ParanoidSense
+
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.face.sense_service=true
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/android.hardware.biometrics.face.xml
+endif
+
 # Overlays
 PRODUCT_PACKAGE_OVERLAYS += \
     $(EXTRA_PATH)/overlay/common
