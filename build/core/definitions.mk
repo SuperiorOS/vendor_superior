a68070d0e1996eb67b7657cc46d3f38239edca2f
Yumi Yukimura <me.cafebabe@gmail.com>
2025-08-25 01:43:51 +0800

    superior: Move build-manifest.xml build rule into `build/tasks`
    * For Android.mk deprecation
    
    Change-Id: I4aba949ae0af7674955d7a2c439d191348b41f3c

--- build/core/definitions.mk
+++ build/core/definitions.mk
@@ -7,3 +7,6 @@ endef
 
 # Include board/platform macros
 include vendor/superior/build/core/utils.mk
+
+# Include default installed modules
+include vendor/superior/build/core/default_installed_modules.mk
--- config/common.mk
+++ config/common.mk
@@ -126,10 +126,6 @@ PRODUCT_PACKAGES += \
     bootanimation.zip \
     bootanimation-dark.zip
 
-# Build Manifest
-PRODUCT_PACKAGES += \
-    build-manifest.xml
-
 # Superior packages
 ifeq ($(PRODUCT_IS_ATV),)
 PRODUCT_PACKAGES += \
