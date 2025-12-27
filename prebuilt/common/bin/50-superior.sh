30294172507844a5a72ca4aa1fb40cbd21af8f34
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-12-27 23:16:20 +0800

    backuptool: renaming addon.d -> backup.tools
    Change-Id: I6d43247063eb74ab96abd8f2fbc0e19f232ae676

--- prebuilt/common/bin/50-superior.sh
+++ prebuilt/common/bin/50-superior.sh
@@ -2,7 +2,7 @@
 #
 # ADDOND_VERSION=2
 #
-# /system/addon.d/50-superior.sh
+# /system/backup.tools/50-superior.sh
 # During a SuperiorOS upgrade, this script backs up /system/etc/hosts,
 # /system is formatted and reinstalled, then the file is restored.
 #
